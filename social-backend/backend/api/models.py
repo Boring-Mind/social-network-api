7274b35b1f9c416ef4d9a0b633c8247cb3504f7f
BoringMind <super.d3280@ya.ru>
2022-12-23 16:59:09 +0400

    feat: added models, migrations and fixed some bugs that doesn't allow us to launch app

--- social-backend/backend/api/models.py
+++ social-backend/backend/api/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    edited_on = models.DateTimeField(auto_now=True)
+
+
+class Like(models.Model):
+    person = models.ForeignKey(User, on_delete=models.CASCADE, related_name="person")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post")
+    created = models.DateTimeField(auto_now_add=True)
--- social-backend/backend/backend/settings.py
+++ social-backend/backend/backend/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-party apps
+    "rest_framework",
+    # Our apps
+    "api",
 ]
 
 MIDDLEWARE = [
@@ -134,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
