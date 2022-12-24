ba95d17672e4e7621c21af562aae1e955fb653d0
BoringMind <super.d3280@ya.ru>
2022-12-24 11:09:43 +0400

    feat: added cache support for Django

--- social-backend/backend/backend/settings.py
+++ social-backend/backend/backend/settings.py
@@ -53,7 +53,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.cache.UpdateCacheMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django.middleware.cache.FetchFromCacheMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -164,3 +166,15 @@ STATIC_ROOT = BASE_DIR / "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://cache:6379",
+        "OPTIONS": {
+            "MAX_ENTRIES": 600,
+            "parser_class": "redis.connection.HiredisParser",
+        },
+        "KEY_PREFIX": "django_api_",
+    }
+}
