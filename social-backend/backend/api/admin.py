d9316b5373c7251d8d3eb73cc834b931bd542d59
BoringMind <super.d3280@ya.ru>
2022-12-25 10:57:03 +0400

    feat: added Like and Post models to admin site

--- social-backend/backend/api/admin.py
+++ social-backend/backend/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Post, Like
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    pass
