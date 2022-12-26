fcb318f605c9e9075a4161959b994ba5e882e426
BoringMind <super.d3280@ya.ru>
2022-12-26 18:49:34 +0400

    feat: added instructions to never cache post views

--- social-backend/backend/api/views.py
+++ social-backend/backend/api/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.views.decorators.cache import never_cache
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
@@ -11,8 +12,16 @@ class UserSignupApiView(CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
 
+    @never_cache
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
 
 class PostCreateApiView(CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (IsAuthenticated,)
+
+    @never_cache
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
