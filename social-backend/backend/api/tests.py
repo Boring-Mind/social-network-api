7f46b68a8e3523c83e77644f42143ea10e085ece
BoringMind <super.d3280@ya.ru>
2023-01-01 18:21:56 +0400

    test: added test case for initadmin command

--- social-backend/backend/api/tests.py
+++ social-backend/backend/api/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from unittest.mock import patch
 
-# Create your tests here.
+import pytest
+from django.contrib.auth.models import User
+from django.core.management import call_command
+
+
+def fixture_superuser_env_variables(env_var_name: str):
+    if env_var_name == "DJANGO_SUPERUSER_USERNAME":
+        return "admin"
+    if env_var_name == "DJANGO_SUPERUSER_EMAIL":
+        return "some.mail@mail.com"
+    if env_var_name == "DJANGO_SUPERUSER_PASSWORD":
+        return "S0m3P@ssw0rd"
+
+
+@pytest.mark.django_db
+@patch(
+    "api.management.commands.initadmin.config",
+    autospec=True,
+    side_effect=fixture_superuser_env_variables,
+)
+def test_initadmin_command__default_case(config_mock):
+    call_command("initadmin")
+
+    assert config_mock.call_count == 3
+
+    user_list = User.objects.all()
+    assert len(user_list) == 1
+
+    user = user_list[0]
+    assert user.username == "admin"
+    assert user.email == "some.mail@mail.com"
